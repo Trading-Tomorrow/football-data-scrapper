c5d8b697b452b34202cac6644dacc40f851ac782
Fabio Zuin <fabiozuin@outlook.com>
2025-12-04 21:29:51 +0000

    feat: Add historical lineups data for multiple seasons and update the formations scraper.

--- src/formations_scraper.py
+++ src/formations_scraper.py
@@ -10,14 +10,14 @@ from selenium.webdriver.support import expected_conditions as EC
 # ----------------------------
 # 1. Load your CSV
 # ----------------------------
-CSV_PATH = "premierleague_2023_24_stats.csv"
+CSV_PATH = "premierleague_2016_17_stats.csv"
 df = pd.read_csv(CSV_PATH)
 
 # Column name – adjust if needed
 GAME_ID_COL = "game_id"
 
 # Output file
-OUTPUT_FILE = "lineups_data_2023_24.csv"
+OUTPUT_FILE = "lineups_data_2016_17.csv"
 
 # ----------------------------
 # 2. Setup Webdriver
